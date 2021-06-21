dc209d105beea8ce996322d4ccdac46301481f58
Philippe Delteil <pdelteil@gmail.com>
2021-06-21 05:59:27 +0000

    nuclei helper functions added

---
+++ nuclei_helper.sh
@@ -0,0 +1,20 @@
+#nuclei heler
+
+# function to manually test results from nuclei
+
+# example of use 
+# testNucleiTemplate open-redirect http://www.sample.com
+testNucleiTemplate()
+{
+ if [ -z "$1" ]
+    then
+      echo "Use ${FUNCNAME[0]} nuclei-template-id URL"
+      return 1;
+    fi
+
+ templateID=$1
+ URL=$2
+ pathToTemplate=$(locate $templateID|grep nuclei-templates) 
+ echo "nuclei -debug -t $pathToTemplate -u $URL"
+ nuclei -debug -t $pathToTemplate -u $URL
+}
