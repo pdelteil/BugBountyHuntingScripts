86dc3a4aeb1d2aae0b5ba081ab35c2b4bd4b624d
Philippe Delteil <pdelteil@gmail.com>
2021-07-11 18:43:05 +0000

    addex axiom helper

---
+++ axiom_helper.sh
@@ -0,0 +1,20 @@
+#axiom helper
+
+#show logs from running axiom scan
+# input param is the module used to scan
+showLogs()
+{
+ if [ -z "$1" ]
+    then
+      echo "Use ${FUNCNAME[0]} axiomModule"
+      return 1;
+    fi
+
+axiomModule=$1
+#find latest folder of given axiomModule
+log_path=$(ls -t ~/.axiom/tmp/$axiomModule* | head -n 1|sed 's/://g')
+cd $log_path
+#nuclei especific grep rules 
+cat $log_path/logs/*|grep "2021-"|grep -v Unsolicited
+
+}
