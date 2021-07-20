0ac00665b876508f75c627ef6050e3d9f8e84773
Philippe Delteil <pdelteil@gmail.com>
2021-07-20 22:50:29 +0000

    added setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+
+#TODO: prompt which helper to install
+location=$(pwd); echo "source $location/BBRF-maintenance/bbrf_helper.sh " >> ~/.bashrc
+location=$(pwd); echo "source $location/BBRF-maintenance/nuclei_helper.sh " >> ~/.bashrc
+location=$(pwd); echo "source $location/BBRF-maintenance/general_helper.sh " >> ~/.bashrc
+
+source ~/.bashrc 
