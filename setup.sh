080d6286bae49e5d3b1fee39001f178c53ed8096
Philippe Delteil <pdelteil@gmail.com>
2021-07-20 22:51:45 +0000

    added setup 2

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 location=$(pwd); echo "source $location/BBRF-maintenance/bbrf_helper.sh " >> ~/.bashrc
 location=$(pwd); echo "source $location/BBRF-maintenance/nuclei_helper.sh " >> ~/.bashrc
 location=$(pwd); echo "source $location/BBRF-maintenance/general_helper.sh " >> ~/.bashrc
+location=$(pwd); echo "source $location/BBRF-maintenance/axiom_helper.sh " >> ~/.bashrc
 
 source ~/.bashrc 
