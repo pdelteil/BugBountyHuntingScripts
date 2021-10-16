5521dae6cc670a0670649564c970fd1c2ac66f50
Philippe Delteil <pdelteil@gmail.com>
2021-10-16 01:18:20 +0000

    getField updated

--- general_helper.sh
+++ general_helper.sh
@@ -1,4 +1,3 @@
-
 #general helper functions 
 #create a screen instance with a given name
 function createScreen()
@@ -66,7 +65,15 @@ locateCat()
 # example:  cat file.txt | getField 4 
 getField() 
 {
-  awk -v number=$1 '{print $number}'
+    if [ -z "$1" ]
+    then
+      echo -e "Use ${FUNCNAME[0]} number (nth column)"
+      echo "Example:  cat file.txt | getField 4"
+      return 1;
+    fi
+    awk -v number=$1 '{print $number}'
+    #this is an attempt to receive the separator as a function parameter 
+    #awk -v column=$1 -v "field=$2" -F\\${field} '{print $column}' 
 }
 #get domain From URL
 getDomainFromURL()
@@ -131,3 +138,36 @@ getCNFromCerts()
     done
 }
 
+# Get all resolving Tls for a given domain name 
+# For example, a program has a scope like companyA.*, we already know some domains companyA.com and companyA.fr, from here we could use all possible TLDs 
+# to find new domains. This function first uses flat country TLDs and in a second iterating tests for com.tld (cases like com.ar, com.br, com.pe, etc)
+getTLDs()
+{
+    URL="https://raw.githubusercontent.com/datasets/top-level-domain-names/master/top-level-domain-names.csv"
+    if [ -z "$1" ] 
+    then
+        echo "Use ${FUNCNAME[0]} targetDomain"
+        echo "Example ${FUNCNAME[0]} testing-sites"
+        return 1;
+    fi
+    FILE="top-level-domain-names.csv"
+
+    if [ -f "$FILE" ]; then
+        echo "$FILE exists."
+    #if file not found download it 
+    else 
+        echo "$FILE does not exist."
+        echo "Downloading ... "
+        curl -ks $URL -o $FILE
+    fi
+
+    target="$1"
+    #do with com 
+    tlds=$(grep country-code "$file" |awk -F "," '{print $1}'|grep -Pv '[^\x00-\x7F]'|awk -v target=$target '{print target$1}' |dnsx | awk '{print "*."$1}')
+    echo "$tlds"
+
+    # test them with com 
+    target="$target".com
+    tlds=$(grep country-code "$file" |awk -F "," '{print $1}'|grep -Pv '[^\x00-\x7F]'|awk -v target=$target '{print target$1}' |dnsx | awk '{print "*."$1}')
+    echo "$tlds"
+}
