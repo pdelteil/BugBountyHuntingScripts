10dd507f5134307a12da3b5c8c8fa06d25313ce2
Deleite <pdelteil@gmail.com>
2022-02-03 05:33:25 +0000

    setup improved

--- setup.sh
+++ setup.sh
@@ -1,30 +1,34 @@
 
 #TODO: prompt which helper to install
-location=$(pwd); echo "source $location/bbrf_helper.sh " >> ~/.bashrc
-location=$(pwd); echo "source $location/nuclei_helper.sh " >> ~/.bashrc
-location=$(pwd); echo "source $location/general_helper.sh " >> ~/.bashrc
-location=$(pwd); echo "source $location/axiom_helper.sh " >> ~/.bashrc
+echo "Starting setup script"
+
 #change this if you don't use bash
 shellConfig="$HOME/.bashrc"
+
 location=$(pwd)
 
 #installing dependencies 
+echo "Installing dependencies..."
 
+echo "Installing subfinder"
 #subfinder
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
 
+echo "Installing httpx" 
 #httpx
 GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
 
+echo "Installing dnsx" 
 #dnsx
 GO111MODULE=on go get -v github.com/projectdiscovery/dnsx/cmd/dnsx
 
+echo "Installing assetfinder" 
 #assetfinder
 GO111MODULE=on go get -u github.com/tomnomnom/assetfinder
 
+echo "Installing httprobe" 
 #httprobe
 go get -u github.com/tomnomnom/httprobe@master
-
 
 echo "Adding bbrf_helper.sh to $shellConfig"
 echo "source $location/bbrf_helper.sh " >> $shellConfig
