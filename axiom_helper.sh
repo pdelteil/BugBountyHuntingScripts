cbdcaf4b4b2b7eb3eaeb104d3d68fd4d2e8dce5f
Deleite <pdelteil@gmail.com>
2022-10-01 01:08:24 +0000

    several fixes

--- axiom_helper.sh
+++ axiom_helper.sh
@@ -7,7 +7,7 @@ showLogs()
  if [ -z "$1" ]
     then
       echo "Use ${FUNCNAME[0]} axiomModule"
-      echo -e "Supported modules\n nuclei\n shuffledns\n puredns"
+      echo -e "Supported modules\n nuclei\n shuffledns\n puredns-bruteforce"
       return 1;
     fi
 
@@ -15,27 +15,43 @@ axiomModule=$1
 #find latest folder of given axiomModule
 log_path=$(ls -td ~/.axiom/tmp/$axiomModule* 2>/dev/null| head -n 1|sed 's/://g')
 log_path=$log_path/logs
+echo "size ${#log_path}"
 
 #TODO add more grep especific rules for other modules
-#nuclei especific grep rules 
 year=$(date +"%Y-")
 
 #nuclei 
+
 if [ $axiomModule = "nuclei" ]
     then
-        cd $log_path
-        cat $log_path/*|grep $year|grep -v Unsolicited
+        if [ ${#log_path} -gt 5 ]
+        then
+            cd $log_path
+            #nuclei especific grep rules 
+            cat $log_path/*|grep $year|grep -v Unsolicited
+        else 
+            echo "nuclei is not running or there are no logs"
+
+        fi
 #shuffledns
 elif [ $axiomModule = "shuffledns" ] 
     then
-        cd $log_path
-        cat $log_path/*|grep -v 'INF\|WRN\|projectdiscovery'|grep '\.'
-
+        if [ ${#log_path} -gt 5 ]
+           then
+               cd $log_path
+               cat $log_path/*|grep -v 'INF\|WRN\|projectdiscovery'|grep '\.'
+        fi
 #puredns
-elif [ $axiomModule = "puredns" ] 
+elif [ $axiomModule = "puredns-bruteforce" ];
     then
-        cd $log_path
-        cat $log_path/*|grep -v 'INF\|WRN\|projectdiscovery'|grep '\.'
+        if [ ${#log_path} -gt 5 ];
+           then
+               cd $log_path
+               cat $log_path/*|grep ETA
+        else 
+              echo "No logs"
+
+        fi
 
 else 
     echo "module $axiomModule not supported"
