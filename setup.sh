c6535ed040849f304afd69952d54bf12f0b501e5
Deleite <pdelteil@gmail.com>
2022-12-17 05:13:21 +0000

    checking if tools are previously installed, formatting, refactoring

--- setup.sh
+++ setup.sh
@@ -9,60 +9,49 @@ echo -ne "${RED}\nStarting setup script${ENDCOLOR}\n\n"
 
 #change this if you don't use bash
 shellConfig="$HOME/.bashrc"
-
 location=$(pwd)
+
+function installTool()
+{
+    cmdname="$1"
+    installcmd="$2"
+    echo -ne "${YELLOW}- Checking for $cmdname ${ENDCOLOR}\n"
+
+    if command -v $cmdname &> /dev/null; then
+        echo -ne "${YELLOW}  $cmdname is installed! Skipping${ENDCOLOR}\n"
+    else
+        echo -ne "${YELLOW}  $cmdname is not installed, installing...${ENDCOLOR}\n"
+        "$installcmd"
+    fi
+}
+
+function addtoBashrc()
+{
+    helper="$1"
+    configfile="$2"
+    echo -ne "${YELLOW}Adding $helper to $configfile${ENDCOLOR}\n"
+    echo "source $location/$helper " >> $configfile
+}
 
 #installing dependencies 
 echo -ne "${YELLOW}Installing dependencies...${ENDCOLOR}\n\n"
 
-echo -ne "${YELLOW}Installing subfinder ${ENDCOLOR}\n"
-#subfinder
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+installTool gcc          "sudo apt install gcc"
+installTool subfinder    "go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest"
+installTool httpx        "go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest"
+installTool dnsx         "go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest"
+installTool assetfinder  "go install -v github.com/tomnomnom/assetfinder@latest"
+installTool httprobe     "go install -v github.com/tomnomnom/httprobe@master"
+installTool gau          "go install -v github.com/lc/gau/v2/cmd/gau@latest"
+installTool waybackurls  "go install -v github.com/tomnomnom/waybackurls@latest"
+installTool amass        "go install -v github.com/OWASP/Amass/v3/...@master"
+installTool unfurl       "go install -v github.com/tomnomnom/unfurl@latest"
 
-echo -ne "${YELLOW}Installing httpx ${ENDCOLOR}\n" 
-#httpx
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-
-echo -ne "${YELLOW}Installing dnsx ${ENDCOLOR}\n" 
-#dnsx
-go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
-
-echo -ne "${YELLOW}Installing assetfinder ${ENDCOLOR}\n" 
-#assetfinder
-go install -v github.com/tomnomnom/assetfinder@latest
-
-echo -ne "${YELLOW}Installing httprobe ${ENDCOLOR}\n" 
-#httprobe
-go install github.com/tomnomnom/httprobe@master
-
-echo -ne "${YELLOW}Installing gau ${ENDCOLOR}\n" 
-#gau 
-go install github.com/lc/gau/v2/cmd/gau@latest
-
-echo -ne "${YELLOW}Installing waybackurls ${ENDCOLOR}\n" 
-#waybackurls
-go install github.com/tomnomnom/waybackurls@latest
-
-echo -ne "${YELLOW}Installing amass ${ENDCOLOR}\n" 
-#amass 
-go install -v github.com/OWASP/Amass/v3/...@master
-
-#unfurl 
-go install -v github.com/tomnomnom/unfurl@latest
-
-echo -ne "${YELLOW}Adding bbrf_helper.sh to $shellConfig${ENDCOLOR}\n"
-echo "source $location/bbrf_helper.sh " >> $shellConfig
-
-echo -ne "${YELLOW}Adding nuclei_helper.sh to $shellConfig${ENDCOLOR}\n"
-echo "source $location/nuclei_helper.sh " >> $shellConfig
-
-echo -ne "${YELLOW}Adding general_helper.sh to $shellConfig${ENDCOLOR}\n"
-echo "source $location/general_helper.sh " >> $shellConfig
-
-echo -ne "${YELLOW}Adding bbrf_helper.sh to $shellConfig${ENDCOLOR}\n"
-echo "source $location/axiom_helper.sh " >> $shellConfig
-
-echo -ne "${YELLOW}Adding amass_helper.sh to $shellConfig${ENDCOLOR}\n"
-echo "source $location/amass_helper.sh " >> $shellConfig
+echo  "" 
+addtoBashrc bbrf_helper.sh $shellConfig
+addtoBashrc nuclei_helper.sh $shellConfig
+addtoBashrc general_helper.sh $shellConfig
+addtoBashrc axiom_helper.sh $shellConfig
+addtoBashrc amass_helper.sh $shellConfig
 
 source $shellConfig
